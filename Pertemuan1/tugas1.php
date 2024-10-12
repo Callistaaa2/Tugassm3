c3bbd55f7e0d4edbaed026aab5418b4d8ff95788
Callistaaa2 <muhamadwildan180@gmail.com>
2024-10-12 18:40:40 +0700

    Add files via upload

---
+++ Pertemuan1/tugas1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas1-0035</title>
+</head>
+<body>
+    <form action="tugas1.php" method="post">
+    <label for="agree">
+        <input type="checkbox" id="agree" name="agree"> Saya setuju dengan syarat dan ketentuan
+    </label>
+    <br>
+    <input type="submit" name="submit" value="Submit">
+
+
+    </form>
+
+
+
+
+
+
+    <?php
+    <input type="checkbox" name="cek" id="cek">
+
+    
+    
+    ?>
+    
+</body>
+</html>
